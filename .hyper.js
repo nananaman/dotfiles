75ecdc7a998ace6317b968e507bd98880afeb1b9
Junta Watanabe <foolawoola@gmail.com>
2020-07-23 01:57:54 +0900

    update: message

--- .hyper.js
+++ .hyper.js
@@ -12,7 +12,6 @@ module.exports = {
     fontSize: 16,
 
     // font family with optional fallbacks
-    // fontFamily: 'HackGen, Ricty for Powerline, Droid Sans Mono Nerd Font Complete, Hack Nerd Font, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
     fontFamily: 'HackGenNerd Console, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
