def687ab0a0e43371895b504e0acb565b5afc9d6
nananaman <foolawoola@gmail.com>
2020-07-23 02:01:11 +0900

    update: hyperのwin対応

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -15,7 +15,12 @@ ln -snfv ~/dotfiles/.vimrc ~/.vimrc
 ln -snfv ~/dotfiles/.xonshrc ~/.xonshrc
 ln -snfv ~/dotfiles/coc-settings.json ~/.vim/coc-settings.json
 ln -snfv ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -snfv ~/dotfiles/.hyper.js ~/.hyper.js
+
+if [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
+  ln -snfv ~/dotfiles/.hyper.win.js /mnt/c/Users/foola/AppData/Roaming/Hyper/.hyper.js
+else
+  ln -snfv ~/dotfiles/.hyper.js ~/.hyper.js
+fi
 
 cat << END
 
