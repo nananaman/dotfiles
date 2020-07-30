08d672baeb767b9ba7b7edc2621488a2929dddeb
nananaman <foolawoola@gmail.com>
2020-07-30 16:09:32 +0900

    update: 色々追加

--- install_fzf.sh
+++ install_fzf.sh
@@ -2,3 +2,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 yes | ~/.fzf/install
 
 echo '[ -f ~/.fzf.bash ] && source ~/.fzf.bash' >> ~/.bashrc
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+cargo install --locked bat
+cargo install lsd
