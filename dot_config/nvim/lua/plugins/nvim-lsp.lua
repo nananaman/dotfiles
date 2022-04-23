7e1f6cbf3ceb9b7642efcc171923c0940786ff6a
nananaman <foolawoola@gmail.com>
2022-04-23 23:56:20 +0900

    add: config for TypeScript

--- dot_config/nvim/lua/plugins/nvim-lsp.lua
+++ dot_config/nvim/lua/plugins/nvim-lsp.lua
@@ -57,6 +57,76 @@ local enhance_server_opts = {
       },
     }
   end,
+  ["jsonls"] = function(opts)
+    opts.filetypes = { "json", "jsonc" }
+    opts.settings = {
+      json = {
+        -- Schemas https://www.schemastore.org
+        schemas = {
+          {
+            fileMatch = { "package.json" },
+            url = "https://json.schemastore.org/package.json"
+          },
+          {
+            fileMatch = { "tsconfig*.json" },
+            url = "https://json.schemastore.org/tsconfig.json"
+          },
+          {
+            fileMatch = {
+              ".prettierrc",
+              ".prettierrc.json",
+              "prettier.config.json"
+            },
+            url = "https://json.schemastore.org/prettierrc.json"
+          },
+          {
+            fileMatch = { ".eslintrc", ".eslintrc.json" },
+            url = "https://json.schemastore.org/eslintrc.json"
+          },
+          {
+            fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
+            url = "https://json.schemastore.org/babelrc.json"
+          },
+          {
+            fileMatch = { "lerna.json" },
+            url = "https://json.schemastore.org/lerna.json"
+          },
+          {
+            fileMatch = { "now.json", "vercel.json" },
+            url = "https://json.schemastore.org/now.json"
+          },
+          {
+            fileMatch = {
+              ".stylelintrc",
+              ".stylelintrc.json",
+              "stylelint.config.json"
+            },
+            url = "http://json.schemastore.org/stylelintrc.json"
+          }
+        }
+      }
+    }
+  end,
+  ["tsserver"] = function(opts)
+    local root_dir = nvim_lsp.util.root_pattern("package.json", "node_modules")
+    opts.root_dir = root_dir
+    opts.autostart = detected_root_dir(root_dir)
+    opts.init_options = { lint = true, unstable = true }
+    opts.on_attach = function(client, bufnr)
+      client.resolved_capabilities.document_formatting = false
+      on_attach(client, bufnr)
+    end
+  end,
+  ["eslint"] = function(opts)
+    local root_dir = nvim_lsp.util.root_pattern("package.json", "node_modules")
+    opts.root_dir = root_dir
+    opts.autostart = detected_root_dir(root_dir)
+    opts.init_options = { lint = true, unstable = true }
+    opts.on_attach = function(client, bufnr)
+      client.resolved_capabilities.document_formatting = false
+      on_attach(client, bufnr)
+    end
+  end,
   ["denols"] = function(opts)
     local root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc", "deps.ts")
     opts.root_dir = root_dir
@@ -89,8 +159,6 @@ lsp_installer.settings({
 })
 
 vim.opt.completeopt = "menu,menuone,noselect"
--- vim.opt.spell = true
--- vim.opt.spelllang = { "en_us" }
 
 -- Setup nvim-cmp.
 local cmp = require("cmp")
@@ -135,7 +203,7 @@ cmp.setup({
     { name = "nvim_lsp_signature_help" },
     { name = "emoji" },
     { name = "calc" },
-    { name = "spell" },
+    { name = "treesitter" },
   }, {
     { name = "buffer" },
   }),
