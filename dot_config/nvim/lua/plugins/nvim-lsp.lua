9f68a8b0d1ae4d1ed3a63ec003decf95f521866f
nananaman <foolawoola@gmail.com>
2023-03-28 02:14:23 +0900

    add: path

--- dot_config/nvim/lua/plugins/nvim-lsp.lua
+++ dot_config/nvim/lua/plugins/nvim-lsp.lua
@@ -237,8 +237,8 @@ return {
           documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-d>"] = cmp.mapping.scroll_docs(4),
           -- ["<Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
