89fa3551e0e9fc6344f123b82af2ef341057b05e
nananaman <foolawoola@gmail.com>
2023-04-27 22:17:46 +0900

    add: copilot-cli 用スクリプト

--- dot_config/fish/conf.d/functions.fish
+++ dot_config/fish/conf.d/functions.fish
@@ -18,3 +18,22 @@ function fvd
     and exec fish
   end
 end
+
+set -U fish_features qmark-noglob
+function github-copilot_helper
+    set -l TMPFILE (mktemp)
+    trap 'rm -f $TMPFILE' EXIT
+    if github-copilot-cli $argv[1] "$argv[2..]" --shellout $TMPFILE
+        if [ -e "$TMPFILE" ]
+            set -l FIXED_CMD (cat $TMPFILE)
+            eval "$FIXED_CMD"
+        else
+            echo "Apologies! Extracting command failed"
+        end
+    else
+        return 1
+    end
+end
+alias ??='github-copilot_helper what-the-shell'
+alias git?='github-copilot_helper git-assist'
+alias gh?='github-copilot_helper gh-assist'
