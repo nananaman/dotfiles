b8ef65ed72d828a91e6ea1f1ad2c8283bc8d7429
nananaman <foolawoola@gmail.com>
2024-01-10 16:04:31 +0900

    🔧 修正(colorizer.lua): colorizerの設定にtomlを追加 🔧 修正(starship.toml): starshipのフォーマットを更新し、コメントアウトを解除しました

--- dot_config/nvim/lua/plugins/colorizer.lua
+++ dot_config/nvim/lua/plugins/colorizer.lua
@@ -1,6 +1,15 @@
 return {
-  'norcalli/nvim-colorizer.lua',
+  "norcalli/nvim-colorizer.lua",
   config = function()
-    require("colorizer").setup({ "css", "javascript", "html", "dart", "vue", "lua", "vim" })
-  end
+    require("colorizer").setup({
+      "css",
+      "javascript",
+      "html",
+      "dart",
+      "vue",
+      "lua",
+      "vim",
+      "toml",
+    })
+  end,
 }
