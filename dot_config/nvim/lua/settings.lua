f9aa197911c91788a1c364611328a16b3ef3e8d0
nananaman <foolawoola@gmail.com>
2024-05-10 14:46:01 +0900

    🔧 修正(settings.lua): 相対行番号の表示を削除 🔧 修正(settings.lua): ステータスラインの表示を常に行うように変更

--- dot_config/nvim/lua/settings.lua
+++ dot_config/nvim/lua/settings.lua
@@ -13,8 +13,6 @@ vim.opt.hidden = true
 vim.opt.termguicolors = true
 -- 行番号を表示
 vim.opt.number = true
--- 相対行番号を表示
-vim.wo.relativenumber = true
 -- 現在の行を強調表示
 vim.opt.cursorline = true
 -- ステータスラインを常に表示. Noice.nvim でコマンドラインは中央に表示している
