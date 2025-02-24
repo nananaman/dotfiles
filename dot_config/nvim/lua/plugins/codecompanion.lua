3974ef3b1e6e244b79af1971a4402b5a0e0950bb
nananaman <foolawoola@gmail.com>
2025-02-24 15:35:26 +0900

    update: CodeCompanionChatを開くkeybindをwindsurfに合わせる

--- dot_config/nvim/lua/plugins/codecompanion.lua
+++ dot_config/nvim/lua/plugins/codecompanion.lua
@@ -47,8 +47,8 @@ return {
 
     vim.api.nvim_set_keymap("n", "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
     vim.api.nvim_set_keymap("v", "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
-    vim.api.nvim_set_keymap("n", "<Leader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
-    vim.api.nvim_set_keymap("v", "<Leader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<C-l>", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("v", "<C-l>", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
     vim.api.nvim_set_keymap("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
 
     -- Expand 'cc' into 'CodeCompanion' in the command line
