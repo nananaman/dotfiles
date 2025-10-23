ee918273867e1776b92efdbdcd91e2c0e2996ddd
nananaman <foolawoola@gmail.com>
2025-10-23 18:13:12 +0900

    fix: lsp

--- dot_config/nvim/after/lsp/pyright.lua
+++ dot_config/nvim/after/lsp/pyright.lua
@@ -1,5 +1,6 @@
 ---@type vim.lsp.Config
 return {
+  root_markers = { "uv.lock", ".git" }, -- pyproject.toml を指定すると uv workspace の場合に各 workspace ごとにサーバーが起動してしまう
   settings = {
     python = {
       venvPath = ".",
